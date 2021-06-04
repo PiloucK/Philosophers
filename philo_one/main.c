abb913a881d4f4772f6d812408d471e66814cb5c
PiloucK <clkuznie@student.42.fr>
2021-06-04 18:29:17 +0200

    .

---
+++ philo_one/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/28 15:05:49 by clkuznie          #+#    #+#             */
+/*   Updated: 2021/06/03 12:06:06 by clkuznie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+typedef struct	s_philo_parameter
+{
+	size_t	philosopher_count;
+	size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+	size_t	meal_count;
+}				t_philo_parameter;
+
+int
+	main(int argc
+	, char **argv)
+{
+	return (EXIT_SUCCESS);
+}
